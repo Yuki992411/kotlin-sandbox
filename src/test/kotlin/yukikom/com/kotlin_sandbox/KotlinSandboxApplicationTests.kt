38b2083ba03cb930fe914ff82f5111e255e0fac5
Yuki992411 <k.yuki.9924.11@gmail.com>
2024-09-02 00:59:51 +0900

    first commit

---
+++ src/test/kotlin/yukikom/com/kotlin_sandbox/KotlinSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package yukikom.com.kotlin_sandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
